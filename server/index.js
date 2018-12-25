c9b711bca82bb2a25dadd617a9d32cb29046f479
Melissa Boyer <melissaboyer3000@gmail.com>
2018-12-25 10:03:52 -0500

    Starting oauth process

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 
-// Route handler
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
-});
+// Create a new instance of the google passport strategy and point passport to it
+passport.use(new GoogleStrategy());
 
 // Allow dynamic port binding for deployment to Heroku
 const PORT = process.env.PORT || 5000;
