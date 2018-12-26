4a06a9496ec96408d6e17c9a8b021b3845941240
Melissa Boyer <melissaboyer3000@gmail.com>
2018-12-26 12:15:10 -0500

    Working on adding new user to db - not working yet

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,6 +1,7 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys');
+const db = require('../dbconnection');
 
 // Create a new instance of the google passport strategy and point passport to it to enable google oAuth
 passport.use(
@@ -11,9 +12,7 @@ passport.use(
 			callbackURL: '/auth/google/callback'
 		}, 
 		(accessToken, refreshToken, profile, done) => {
-			console.log('access token', accessToken);
-			console.log('refresh token', refreshToken);
-			console.log('profile:', profile);
+			db.createUser(profile.id);
 		}
 	)
 );
