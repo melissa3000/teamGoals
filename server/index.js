98069df84cae538cd4104ade0cfd1c0d703cc5a9
Melissa Boyer <melissaboyer3000@gmail.com>
2018-12-26 11:16:49 -0500

    Refactored to separate db connection from index.js

--- server/index.js
+++ server/index.js
@@ -2,19 +2,8 @@ const express = require('express');
 const keys = require('./config/keys');
 const mysql = require('mysql');
 require('./services/passport');
+require('./dbconnection');
 
-// connect to mysql db
-let con = mysql.createConnection({
-	host: 'localhost',
-	user: keys.dbUser,
-	password: keys.dbPassword,
-	database: 'teamgoals'
-});
-
-con.connect(function(err) {
-	if (err) throw err;
-	console.log('Connected to db');
-});
 
 const app = express();
 
