08cb4999e13c164880bb4f3753584bff0c0add71
Melissa Boyer <melissaboyer3000@gmail.com>
2018-12-26 10:45:58 -0500

    Connected to mySQL db

--- server/index.js
+++ server/index.js
@@ -1,5 +1,20 @@
 const express = require('express');
+const keys = require('./config/keys');
+const mysql = require('mysql');
 require('./services/passport');
+
+// connect to mysql db
+let con = mysql.createConnection({
+	host: 'localhost',
+	user: keys.dbUser,
+	password: keys.dbPassword,
+	database: 'teamgoals'
+});
+
+con.connect(function(err) {
+	if (err) throw err;
+	console.log('Connected to db');
+});
 
 const app = express();
 
