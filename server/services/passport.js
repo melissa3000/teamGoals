2f9bf20b63b531cc5c2791133d7c6bf3181cb206
Melissa Boyer <melissaboyer3000@gmail.com>
2018-12-28 12:18:26 -0500

    checks for existing user and adds new user successfully

--- server/services/passport.js
+++ server/services/passport.js
@@ -3,6 +3,7 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys');
 const db = require('../dbconnection');
 
+
 // Create a new instance of the google passport strategy and point passport to it to enable google oAuth
 passport.use(
 	new GoogleStrategy(
@@ -11,10 +12,55 @@ passport.use(
 			clientSecret: keys.googleClientSecret,
 			callbackURL: '/auth/google/callback'
 		}, 
+		// (profile, done) => {
+		// 	// db.userExists
+		// 	console.log(profile.id);
+		// 	db.con.query("SELECT * FROM users WHERE googleId='"+profile.id+"'", function(err, rows) {
+		// 		console.log(rows);
+		// 		if (err){
+		// 			console.log('error looking for record');
+		// 			// return done(err);
+		// 		} else {
+		// 			console.log(`Adding new user`)
+		// 			let newUser = new Object;
+		// 			newUserID = profile.id;
+		// 			let user_details = { googleId: googleId }
+		// 				db.con.query('INSERT INTO users SET ?', user_details, (err, res) => {
+		// 					if (err) throw err;
+		// 					console.log('new user added successfully');
+		// 					// return done(null, newUser)
+		// 			})
+		// 		}
+		// 	})
+		// }
 		(accessToken, refreshToken, profile, done) => {
-			// console.log(profile.id);
-			db.userExists(profile.id)
+			console.log(profile.id);
+			db.con.query("SELECT * FROM users WHERE googleId='"+profile.id+"'", function(err, rows) {
+				console.log(rows);
+				if (rows.length === 0) {
+					console.log('Adding new user')
+					let user_details = { googleId: profile.id }
+					db.con.query('INSERT INTO users SET ?', user_details, (err, res) => {
+						console.log('new user added')
+					})
+				}
+			});
+			
+				// if (rows.length === 0) {
+				// 	let user_details = { googleId: googleId }
+				// 	db.con.query('INSERT INTO users SET ?', user_details, (err, res) => {
+				// 		// if (err) throw err;
+				// 		console.log('new user added successfully');
+				// 	}
+				// }
+			
+				// .then(user => db.userExists(user));
+			// db.userExists(profile.id);			
 			// db.createUser(profile.id);
+
+			// TODO: call done function when user verification is working:
+			// syntax is done(errors, userRecord)
+
 		}
 	)
 );
