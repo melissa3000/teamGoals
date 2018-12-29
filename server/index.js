4b740069446601a89d8d015b35e5c65c78ab4b18
Melissa Boyer <melissaboyer3000@gmail.com>
2018-12-29 11:26:56 -0500

    Added session cookie for user

--- server/index.js
+++ server/index.js
@@ -1,11 +1,22 @@
 const express = require('express');
-// const passport = require('passport');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
 require('./services/passport');
 require('./dbconnection');
 
 const app = express();
-// app.use(passport.initialize());
-// app.use(passport.session());
+
+app.use(
+	// cookie expires in 30 days and will be automatically encrypted
+	cookieSession({ 
+		maxAge: 30 * 24 * 60 * 60 * 1000,
+		keys: [keys.cookieKey]
+	})
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
 // require function from authRoutes and call it with app to allow routes to access app and run routes
 require('./routes/authRoutes')(app);
 
