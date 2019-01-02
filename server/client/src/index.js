4bfa967b2ff9715ac51a6aa7c05b9422fb382d04
Melissa Boyer <melissaboyer3000@gmail.com>
2019-01-01 20:04:25 -0500

    Start setting up redux

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/App';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+// create a new instance of the redux store
+const store = createStore(() => [], {}, applyMiddleware());
+
+// connect redux store to react side of application by using the provider tag.
+// Set <App /> up as a child to the provider tag. Provider is a react component that knows 
+// how to read changes from the redux store.
+ReactDOM.render(
+	<Provider store={store}><App /></Provider>,
+	document.querySelector('#root')
+);
