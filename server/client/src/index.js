fbf0df38f349130cc75f0f0521b10d0d3aa482dd
Melissa Boyer <melissaboyer3000@gmail.com>
2019-01-03 20:14:10 -0500

    Add proxy for current_user route

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -3,12 +3,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import reduxThunk from 'redux-thunk';
 
 import App from './components/App';
 import reducers from './reducers';
 
 // create a new instance of the redux store
-const store = createStore(reducers, {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 // connect redux store to react side of application by using the provider tag.
 // Set <App /> up as a child to the provider tag. Provider is a react component that knows 
