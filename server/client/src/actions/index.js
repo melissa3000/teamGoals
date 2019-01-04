b1b304a66fc04e6873e68e6030dbe71e3f653b05
Melissa Boyer <melissaboyer3000@gmail.com>
2019-01-03 20:58:03 -0500

    wire up fetchUser action so app knows user is logged in

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -1,6 +1,18 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
 
-const fetchUser = () => {
-	axios.get('/api/current_user');
+
+export const fetchUser = () => async dispatch => {
+	const res = await axios.get('/api/current_user');
+
+	dispatch({ type: FETCH_USER, payload: res })
 };
+
+
+// action creator before refactor, does the same as the code immediately above
+// export const fetchUser = () => {
+// 	return function(dispatch) {
+// 		axios.get('/api/current_user')
+// 			.then(res => dispatch({ type: FETCH_USER, payload: res }))
+// 	};
+// };
