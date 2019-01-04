4afb2a6f3bc0399b87215117b3978a00b9850b7c
Melissa Boyer <melissaboyer3000@gmail.com>
2019-01-04 09:55:02 -0500

    Add landing page component

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -4,9 +4,9 @@ import { connect } from 'react-redux';
 import * as actions from '../actions';
 
 import Header from './Header';
+import Landing from './Landing';
 const Goals = () => <h2>Goals</h2>
 const GoalsNew = () => <h2>GoalsNew</h2>
-const Landing = () => <h2>Landing</h2>
 const Teams = () => <h2>Teams</h2>
 const TeamsNew = () => <h2>TeamsNew</h2>
 
---
+++ server/client/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Landing = () => {
+	return (
+		<div style={{ textAlign: 'center' }}>
+			<h1>
+				Welcome to Team Goals!
+			</h1>
+			Make some goals for yourself
+		</div>
+	);
+};
+
+export default Landing;
